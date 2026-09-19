4b3ac541c313964efafe5a614f554303b74ce5f9
Hariharan <hariharan0467@gmail.com>
2026-09-19 23:19:26 +0530

    For Netlify, keep it as root /. so updated the  vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/portfolio",
+  base: process.env.VITE_BASE_PATH || "/",
 });
